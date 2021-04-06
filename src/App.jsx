caea0d68b4feebe82d88f429d65747acd61547e3
Tilanka_Mihingu <tilankamihingu2018@gmail.com>
2021-04-06 12:03:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import Footer from './Footer';
+import SearchPage from './SearchPage';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+    <Router>
+
+      <Header />
+
+      <Switch>
+      <Route path="/search">
+      <SearchPage />
+      </Route>
+      <Route path="/">
+      <Home />
+      </Route>
+      </Switch>
+      <Footer />
+
+    </Router>
+    </div>
+  );
+}
+
+export default App;
